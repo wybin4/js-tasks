7ffdd0e87875d4daa00437107de3a5dd58325827
ajdivotf <91722656+ajdivotf@users.noreply.github.com>
2022-07-09 17:19:19 +0300

    Create caesars cipher.js

---
+++ caesars cipher.js
@@ -0,0 +1,14 @@
+function rot13(str) {
+  let codes = str.split('').map(char => char.charCodeAt(0));
+  let index;
+  for(let i = 0; i < codes.length; i++)
+    if (codes[i] >= 65 && codes[i] <= 90){
+      if (codes[i] <= 77)
+      	codes[i] += 13;
+      else codes[i] -= 13;
+    }
+  codes = codes.map(val => String.fromCharCode(val));
+  return codes.join('');
+}
+
+console.log(rot13("SERR CVMMN!"));
